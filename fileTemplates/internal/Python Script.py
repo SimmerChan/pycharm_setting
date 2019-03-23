7bcad3604d98a156f2870839b96927329524b646
SimmerChan <7698590@qq.com>
2019-03-23 12:51:26 +0800

    PY-2018.3.5 <Dell@Dell-PC Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, git.xml, project.default.xml, diff.xml, other.xml, Default.xml, code.style.schemes, ui.lnf.xml, filetypes.xml, keymap.xml, Python Script.py, code.style.schemes.xml, colors.scheme.xml, External Tools.xml, databaseDrivers.xml, github.xml, simmerchan.icls, Default.xml, laf.xml, jdk.table.xml, Default _1_.xml, web-browsers.xml, wsl.distributions.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,19 @@
+# encoding=utf-8
+
+"""
+
+@author: SimmerChan
+
+@github: https://github.com/SimmerChan
+
+@zhihu: https://www.zhihu.com/people/simmerchan/
+
+@wechat: 尘世美小茶馆
+
+@file: ${NAME}.py
+
+@time: ${DATE} ${TIME}
+
+@desc:
+
+"""
